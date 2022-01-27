6d79ed44b3baf3080a5a88ab164d5800f71bf515
Florian Cullmann <florian@fcullmann.com>
2022-01-27 23:30:59 +0100

    update state from parent if child slider changes value

--- src/components/Slider_RGB.js
+++ src/components/Slider_RGB.js
@@ -2,22 +2,12 @@ import React from 'react';
 import Slider, { Range } from 'rc-slider';
 
 class Slider_RGB extends React.Component {
-    state = {
-        value: 0
-    }
-
-    handle_change = (value) => {
-        this.setState({ value: value });
-    };
-
     render() {
-        console.log(this.state);
         return (
             <Slider
                 max="255"
-                id={ "slider_" + this.props.color }
-                aria-labelledby="label"
-                onChange={this.handle_change}
+                id={this.props.id}
+                onChange={(e) => this.props.change_state(this.props.id, e)}
             />
         );
     }
--- src/components/Sliders.js
+++ src/components/Sliders.js
@@ -2,12 +2,31 @@ import React from 'react';
 import Slider_RGB from './Slider_RGB';
 
 class Sliders extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.change_state = this.change_state.bind(this);
+
+        this.state = {
+            r: 0,
+            g: 0,
+            b: 0
+        }
+    }
+
+    change_state(color, value) {
+        this.setState({[color]: value});
+
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div>
-                <Slider_RGB color="r" />
-                <Slider_RGB color="g" />
-                <Slider_RGB color="b" />
+                <Slider_RGB id="r" change_state={this.change_state}/>
+                <Slider_RGB id="g" change_state={this.change_state}/>
+                <Slider_RGB id="b" change_state={this.change_state}/>
             </div>
         );
     }
